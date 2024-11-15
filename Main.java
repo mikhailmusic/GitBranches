e20e4b237b3731c0c7f20a9b6e2a2d98d4666dc4
Mikhail Alekseev <mishaalekseev2004@mail.ru>
2024-11-15 10:32:58 +0300

    add addition

--- Main.java
+++ Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
     }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
 }
