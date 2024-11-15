14b0160687c3f50f59d45a9c72931b5440e10a47
Mikhail Alekseev <mishaalekseev2004@mail.ru>
2024-11-15 11:20:43 +0300

    add input first number

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
-        System.out.print("Это добавлено в ветке dev");
+        System.out.println("Это добавлено в ветке dev");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Введите первое число: ");
+        int first = Integer.parseInt(sc.nextLine());
     }
 }
