ed1be0390665d2d299a8ae66800482975c7613ad
Mikhail Alekseev <mishaalekseev2004@mail.ru>
2024-11-15 10:38:47 +0300

    add division

--- Main.java
+++ Main.java
@@ -14,4 +14,11 @@ public class Main {
     public int multiply(int a, int b) {
         return a * b;
     }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Деление на ноль недопустимо");
+        }
+        return a / b;
+    }
 }
